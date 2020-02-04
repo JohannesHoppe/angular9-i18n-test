e9dc8ef7b771e35b969b9020112cb83edba1fb49
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-02-04 13:03:55 +0100

    i18n: Loading the translations at runtime

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { enableProdMode } from '@angular/core';
+import { loadTranslations } from '@angular/localize';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { getTranslations, ParsedTranslationBundle } from '@locl/core';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +10,16 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+const messages = '/assets/i18n/messages.de.json';
+
+getTranslations(messages).then(
+  (data: ParsedTranslationBundle) => {
+    loadTranslations(data.translations);
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  }
+);
